f858b317bc3dd0bd442ebde33f3fe9df1a397a5e
Rahul181007 <rahulsheeraj@gmail.com>
2026-04-16 14:42:12 +0530

    fix: cors for production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,9 +15,11 @@ const app=express()
 
 app.use(express.json());
 app.use(cors({
-  origin:env.FRONTEND_URL,
-  credentials:true
-  
+  origin: [
+    "http://localhost:5173",   
+    env.FRONTEND_URL           
+  ],
+  credentials: true
 }));
 
 app.use(cookieParser());
