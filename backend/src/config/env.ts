b313017dae84f9f999dc60b97e9aaeef4b998697
Rahul181007 <rahulsheeraj@gmail.com>
2026-04-18 10:28:39 +0530

    test ci cd

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,7 +1,11 @@
 import dotenv from "dotenv";
 import { z } from "zod";
 
-dotenv.config();
+import path from "path";
+
+dotenv.config({
+  path: path.resolve(__dirname, "../../.env"),
+});
 
 /* ================= Schema ================= */
 
