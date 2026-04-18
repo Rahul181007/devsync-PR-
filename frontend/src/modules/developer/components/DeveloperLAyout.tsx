a89a4ccbd1d7435ddd4bb69e7fc4b77f7734d182
Rahul181007 <rahulsheeraj@gmail.com>
2026-04-18 11:31:13 +0530

    Frontend updates (UI fixes / feature)

--- frontend/src/modules/developer/components/DeveloperLAyout.tsx
+++ frontend/src/modules/developer/components/DeveloperLAyout.tsx
@@ -22,23 +22,20 @@ const DeveloperLayout = () => {
   }, [user, company, dispatch]);
 
   return (
-    <div className="flex h-screen overflow-hidden">
-
-      <Sidebar 
-        isOpen={isSidebarOpen} 
-        onClose={closeSidebar} 
+    <div className="flex h-screen overflow-hidden bg-gray-50">
+      <Sidebar
+        isOpen={isSidebarOpen}
+        onClose={closeSidebar}
       />
 
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
         <TopBar onMenuClick={openSidebar} />
-
-        <main className="flex-1 bg-gray-50 overflow-y-auto overflow-x-hidden">
-          <div className="w-full">
+        <main className="flex-1 overflow-y-auto overflow-x-hidden">
+          <div className="w-full mx-auto">
             <Outlet />
           </div>
         </main>
       </div>
-
     </div>
   );
 };
